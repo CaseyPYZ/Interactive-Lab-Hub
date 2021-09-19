9c39303e8fe3d3dd7a943bb481b7380664a01f3f
CaseyPan <casey.yzpan@gmail.com>
2021-09-19 17:49:38 -0400

    feat: lab2 part d2f

--- Lab 2/README.md
+++ Lab 2/README.md
@@ -191,6 +191,7 @@ After that, Git will ask you to login to your GitHub account to push the updates
 ## Make a short video of your modified barebones PiClock
 
 \*\*\***Take a video of your PiClock.**\*\*\*
+[VIDEO LINK](https://youtu.be/CRNJnywrDUs)
 
 ## Part G. 
 ## Sketch and brainstorm further interactions and features you would like for your clock for Part 2.
--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -1,9 +1,11 @@
+import datetime
 import time
 import subprocess
 import digitalio
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
+import random
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -50,6 +52,8 @@ bottom = height - padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
+CLOCK_COLOR = "#00C4AA"
+
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
@@ -60,12 +64,48 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+def get_random_ellipse_coord():
+    # x0 = round(random.uniform(3, width - 3.2), 1)
+    # y0 = round(random.uniform(3, height - 3.2), 1)
+    x0 = random.randint(0, width - 1)
+    y0 = random.randint(0, height - 1)
+    x1 = x0 + 1
+    y1 = y0 + 1
+    print(x0, y0, x1, y1)
+    return [x0, y0, x1, y1]
+
+
+# Setup
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+# Time passed in today before clock activated
+now = datetime.datetime.now(tz=None)
+midnight = now.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+seconds = (now - midnight).seconds
+
+# for s in range(seconds):
+#     draw.ellipse(get_random_ellipse_coord(), fill=CLOCK_COLOR, outline=None)
+
+counter = 0
+
 while True:
     # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    #draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    ### Barebones Clock
+    #TIME = time.strftime("%m/%d/%Y %H:%M:%S")
+    # TIME = str(seconds)
+    # y = top
+    # draw.text((x,y), TIME, font=font, fill="#FFFFFF")
+    ### End of Part D
 
+
+    draw.ellipse(get_random_ellipse_coord(), fill=CLOCK_COLOR, outline=None)
+
+    print(counter)
+    counter+=1
+    
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
