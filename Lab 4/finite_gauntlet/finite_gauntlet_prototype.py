55d0d6d534300e2f6dbfad1c47606d6b9a381bb9
CaseyPan <casey.yzpan@gmail.com>
2021-10-25 14:41:41 -0400

    feat: t9 input & speak

--- Lab 4/finite_gauntlet/finite_gauntlet_prototype.py
+++ Lab 4/finite_gauntlet/finite_gauntlet_prototype.py
@@ -8,7 +8,7 @@
 
 import time
 import subprocess
-import itertools
+import t9_imitation as t9
 
 from PIL import Image, ImageDraw, ImageFont
 
@@ -57,14 +57,21 @@ bottom = height - padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
-
 # Load default font.
 font = ImageFont.load_default()
+
+# Speak
+def speak(instruction):
+    command = """
+        say() { 
+            local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; 
+        } ; 
+    """ + f"say '{instruction}'"
+    subprocess.call(command, shell=True)
 
 # Get gesture from APDS9960
 def get_gesture():
     gesture = apds.gesture()
-
     if gesture == 0x03:
         return "LEFT"
     elif gesture == 0x04:
@@ -72,6 +79,68 @@ def get_gesture():
 
     return False
 
+# Input buffers
+NUM_LST = [] # global
+WORD_LST = []
+TEMP_WORD = ""
+SENTENCE = []
+
+# Cap values: 8/9/10/11
+def get_t9_input(TEMP_WORD, WORD_LST, SENTENCE):    
+    
+    # Capacitive sensor values
+    caps = [False] * 4
+    
+    # Check capacitive sensor values
+    for i in range(4):
+        if mpr121[ i + 8 ].value:
+            caps[i] = True
+    
+    if caps[0] and caps[1] and caps[2] and caps[3]:
+        # space: push current word to SENTENCE & clear NUM_LST
+        if len(WORD_LST) > 0:
+            SENTENCE.append(WORD_LST[0])
+        else:
+            SENTENCE.append(TEMP_WORD)
+        NUM_LST.clear()
+    elif caps[1] and caps[2] and caps[3]: 
+        # 1: '
+        NUM_LST.append(1)
+    elif caps[0] and caps[1] and caps[2]: 
+        # 9
+        NUM_LST.append(9)
+    elif caps[2] and caps[3]: 
+        # 8
+        NUM_LST.append(8)
+    elif caps[1] and caps[2]: 
+        # 7
+        NUM_LST.append(7)
+    elif caps[0] and caps[1]: 
+        # 6
+        NUM_LST.append(6)
+    elif caps[3]:
+        # 5
+        NUM_LST.append(5)
+    elif caps[2]:
+        # 4
+        NUM_LST.append(4)
+    elif caps[1]:
+        # 3
+        NUM_LST.append(3)
+    elif caps[0]:
+        # 2
+        NUM_LST.append(2)
+
+    # Get current word list
+    if len(NUM_LST) > 0:
+        WORD_LST = t9.try_it(NUM_LST)
+        if len(WORD_LST) > 0:
+            TEMP_WORD = WORD_LST[0][:len(NUM_LST)]
+            print("NUM LST> ", NUM_LST, "TEMP_WORD>", TEMP_WORD, "WORD LST> ", WORD_LST)
+        else:
+            print("NUM LST> ", NUM_LST, " No possible word")
+
+    return TEMP_WORD, WORD_LST, SENTENCE
 
 
 while True:
@@ -79,25 +148,35 @@ while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    # Text for display
-    CAP_TOUCHED = -1
-
     # Check capacitive sensor values
-    for i in range(12):
-        if mpr121[i].value:
-            CAP_TOUCHED = i
+    TEMP_WORD, WORD_LST, SENTENCE = get_t9_input(TEMP_WORD, WORD_LST, SENTENCE)
 
     # Display text on OLED screen
-    if CAP_TOUCHED != -1:
-        draw.text((x, top + 8), "CAP> " + str(CAP_TOUCHED) + " touched!", font=font, fill=255)
+    draw.text((x, top + 0), TEMP_WORD, font=font, fill=255)
+    draw.text((x, top + 10), ', '.join(WORD_LST), font=font, fill=255)
+    draw.text((x, top + 20), ' '.join(SENTENCE), font=font, fill=255)
 
-    # Get gesture
-    GESTURE = get_gesture()
-    if GESTURE:
+    # Speak if SENTENCE & RIGHT
+    if len(SENTENCE) > 0:
+        # Get gesture
+        GESTURE = get_gesture()
+        # print(GESTURE)
+        if GESTURE and GESTURE == "RIGHT":
+            # Speak
+            speak(' '.join(SENTENCE))
+            # Clear input buffer
+            TEMP_WORD = ""
+            SENTENCE.clear()
+
+    # Backspace
+    if len(TEMP_WORD) > 0:
+        GESTURE = get_gesture()
         print(GESTURE)
-        draw.text((x, top + 16), "GEST> " + GESTURE + " !", font=font, fill=255)
+        if GESTURE and GESTURE == "LEFT":
+            print("backspace")
+            NUM_LST.pop(-1)
 
     # Display image.
     disp.image(image)
     disp.show()
-    time.sleep(0.1)
+    time.sleep(0.5)
--- Lab 4/finite_gauntlet/t9_imitation.py
+++ Lab 4/finite_gauntlet/t9_imitation.py
@@ -3,39 +3,31 @@ import re
 import itertools 
 
 all_words=Counter()
-n2l={2:'abc',3:'def',4:'ghi',5:'jkl',6:'mno',7:'pqrs',8:'tuv',9:'wxyz'}
+n2l={1:'\'', 2:'abc',3:'def',4:'ghi',5:'jkl',6:'mno',7:'pqrs',8:'tuv',9:'wxyz'}
+
+
 with open('./dict/words.txt','r') as di:  # UNIX 250k unique word list 
      all_words.update({line.strip() for line in di if len(line) < 6}) 
 
-with open('./dict/pg1661.txt','r') as fin:   # http://www.gutenberg.org/ebooks/1661.txt.utf-8
+with open('./dict/x_train.txt','r') as fin:  
     for line in fin:
          all_words.update([word.lower() for word in re.findall(r'\b\w+\b',line)])
 
-def combos(*nums):
+def combos(nums):
     t=[n2l[i] for i in nums]
     return tuple(''.join(t) for t in itertools.product(*(t)))
 
-def t9(*nums):
-    combo=combos(*nums)
-    c1=combos(nums[0])
+def t9(nums):
+    combo=combos(nums)
+    c1=combos([nums[0]])
     first_cut=(word for word in all_words if word.startswith(c1))
     return (word for word in first_cut if word.startswith(combo))
 
-def try_it(*nums):
-    s=set(t9(*nums))
-    n=10
-    print('({}) produces {:,} words. Top {}:'.format(','.join(str(i) for i in nums),
-            len(s),min(n,len(s))))
+def try_it(nums):
+    s=set(t9(nums))
+    result = []
     for i, word in enumerate(
           [w for w in sorted(all_words,key=all_words.get, reverse=True) if w in s],1):
-        if i<=n:
-            print ('\t{:2}:  "{}" -- weighted {}'.format(i, word, all_words[word]))
-
-    print()        
-
-try_it(2)
-try_it(2,3)
-try_it(2,3,4)
-try_it(2,3,3,4)
-try_it(6,6,8,3)   
-try_it(2,3,3,4,5) 
+        if i<=5:
+            result.append(word)
+    return result
