aedd1a5555882d550e461f709ace5bb557d91778
Ethan <nahtec@gmail.com>
2021-12-07 00:27:18 -0500

    update: flex sensor code

--- FinalProject/test_fsensor_write.py
+++ FinalProject/test_fsensor_write.py
@@ -5,9 +5,21 @@ import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 import sys
+import threading
 
 import paho.mqtt.client as mqtt
 import uuid
+
+
+SERVO_PINS = [11, 15, 29, 31, 33]
+
+MCP_PIN_DICT = {
+    11: MCP.P0,
+    15: MCP.P1,
+    29: MCP.P2,
+    31: MCP.P3,
+    33: MCP.P4
+}
 
 # Set up MQTT connection
 def set_mqtt_connection():
@@ -22,46 +34,62 @@ def set_mqtt_connection():
     return client
 
 
+class FlexSensorThread(threading.Thread):
+    def __init__(self, name, mcp, servo_pin, mqtt_client, mqtt_topic):
+        threading.Thread.__init__(self, name=name, daemon=False)
+        self.name = name
+        self.s_pin = servo_pin
+        self.mcp = mcp
+        # self.servo = self.GPIO.PWM(self.pin, 50)
+        self.channel = AnalogIn(self.mcp, MCP_PIN_DICT[self.s_pin])
+        # self.servo = servo
+        # self.bent = False
+        self.mqtt_client = mqtt_client
+        self.write_topic = mqtt_topic
+
+    def run(self):
+        print(self.name + " thread started")
+        while(True):
+            volt = self.channel.voltage
+            print(self.name + ' ADC Voltage: ' + str(volt) + 'V')
+            val = "F"
+            if volt > 0.5 + VOLT_THRESHOLD:
+                val = "T"
+
+            self.mqtt_client.publish(self.write_topic, val)
+            print(self.name + " written> " + val)
+
+            time.sleep(0.5)
+
+
+FSENSOR_NUM = 5
+fsensor_threads = []
+
+VOLT_THRESHOLD = 0.2
+
 if __name__ == '__main__':
-    # Servo Controller vars
-    SERVO_CHANNEL_NUM = 16
-    SERVO_PORT_1 = 0
-    SERVO_PORT_2 = 15
 
     # MQTT Topics
-    READ_FROM = 'B'
+    READ_FROM = 'A'
     WRITE_TO = 'A'
-    READ_TOPIC = 'IDD/kcp/talking_ps/' + READ_FROM
-    WRITE_TOPIC = 'IDD/kcp/talking_ps/' + WRITE_TO
+    READ_TOPIC = 'IDD/kcp/talking_ps/' + READ_FROM + '/'
+    WRITE_TOPIC = 'IDD/kcp/talking_ps/' + WRITE_TO + '/'
 
 
     spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
     cs = digitalio.DigitalInOut(board.D5)
     mcp = MCP.MCP3008(spi, cs)
-    channel = AnalogIn(mcp, MCP.P0)
+    # channel = AnalogIn(mcp, MCP.P0)
 
-    VOLT_THRESHOLD = 0.1
     
-
     try:
-        mqtt_client = set_mqtt_connection()
+        for i in range(FSENSOR_NUM):
+            mqtt_client = set_mqtt_connection()
+            fsensor_thread = FlexSensorThread('fsensor'+str(i), mcp, SERVO_PINS[i], mqtt_client, WRITE_TOPIC+str(i))
+            fsensor_threads.append(fsensor_thread)
 
-        while(True):
-            # print('Raw ADC Value: ', channel.value)
-            # print('ADC Voltage: ' + str(channel.voltage) + 'V')
-            
-            volt = channel.voltage
-            print('ADC Voltage: ' + str(volt) + 'V')
-            val = "F"
-
-            if volt < 0.5 - VOLT_THRESHOLD:
-                val = "T"
-
-            mqtt_client.publish(WRITE_TOPIC, val)
-            print("Written to MQTT client> " + val)
-
-            time.sleep(0.5)
-            
+        for i in range(FSENSOR_NUM):
+            fsensor_threads[i].start()
 
 
 
