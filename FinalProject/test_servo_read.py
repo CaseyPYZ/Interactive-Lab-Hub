42864ba37966b20de09ff8433d0871b6850d8ddc
CaseyPan <casey.yzpan@gmail.com>
2021-12-06 19:30:36 -0500

    update: servo code

--- FinalProject/test_servo_read.py
+++ FinalProject/test_servo_read.py
@@ -19,35 +19,47 @@ def set_mqtt_connection():
     
     return client
 
+# Degree  | Duty cycle percentage
+# -90     | 5
+# neutral | 7.5
+# +90     | 10
 
 class ServoThread(threading.Thread):
-    def __init__(self, name, servo, mqtt_client, mqtt_topic):
+    def __init__(self, name, gpio, servo_pin, mqtt_client, mqtt_topic):
         threading.Thread.__init__(self, name=name, daemon=False)
         self.name = name
-        # self.pin = servo_pin
-        # # self.GPIO = gpio
-        # # self.GPIO.setup(self.pin, self.GPIO.OUT)
-        # # self.servo = self.GPIO.PWM(self.pin, 50)
-        # GPIO.setup(self.pin, GPIO.OUT)
+        self.pin = servo_pin
+        self.GPIO = gpio
+        self.GPIO.setup(self.pin, self.GPIO.OUT) # setup GPIO PIN
+        self.servo = self.GPIO.PWM(self.pin, 50)
 
-        self.servo = servo
+        # self.servo = servo
+        self.bent = False
         self.mqtt_client = mqtt_client
         self.read_topic = mqtt_topic
 
+    def set_angle(self, angle):
+        duty = angle / 18 + 2
+        print("duty> ", duty)
+        self.GPIO.output(self.pin, True)
+        self.servo.ChangeDutyCycle(duty)
+        time.sleep(1)
+        self.GPIO.output(self.pin, False)
+        self.servo.ChangeDutyCycle(0)
+        
     def on_message(self, client, userdata, msg):
         msg = msg.payload.decode('UTF-8')
-        print("Servo reads> ", msg)
+        print("Servo reads> ", msg, "servo.bent> ", self.bent)
         if msg == 'T':
-            duty = 2
-            while duty <= 17:
-                self.servo.ChangeDutyCycle(duty)
-                time.sleep(0.1)
-                duty = duty + 1
-            # self.servo.ChangeDutyCycle(16)
-            # time.sleep(0.5)
+            # Bend finger
+            if self.bent == False:
+                self.set_angle(180)
+                self.bent = True
         elif msg == 'F':
-            self.servo.ChangeDutyCycle(0)
-            # time.sleep(0.5)
+            # Unbend finger
+            if self.bent == True:
+                self.set_angle(0)
+                self.bent = False
 
     def run(self):
         self.servo.start(0)
@@ -58,58 +70,33 @@ class ServoThread(threading.Thread):
         self.mqtt_client.loop_forever()
 
 
-
-# servo.start(0)
-# print ("Waiting for 1 second")
-# time.sleep(1)
-
-# print ("Rotating at intervals of 12 degrees")
-# duty = 2
-# while duty <= 17:
-#     servo.ChangeDutyCycle(duty)
-#     time.sleep(1)
-#     duty = duty + 1
-
-# print ("Turning back to 0 degrees")
-# servo.ChangeDutyCycle(2)
-# time.sleep(1)
-# servo.ChangeDutyCycle(0)
-
-# servo.stop()
-# GPIO.cleanup()
-# print ("Everything's cleaned up")
-
+# Servo Controller vars
+SERVO_NUM = 5
+SERVO_PINS = [11, 15, 29, 31, 33]
+servo_threads = []
 
 
 if __name__ == '__main__':
-    # Servo Controller vars
-    SERVO_PIN = 11
 
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(SERVO_PIN, GPIO.OUT)
-    servo = GPIO.PWM(SERVO_PIN, 50)
-
     
     # MQTT Topics
     READ_FROM = 'A'
     WRITE_TO = 'A'
     READ_TOPIC = 'IDD/kcp/talking_ps/' + READ_FROM
     WRITE_TOPIC = 'IDD/kcp/talking_ps/' + WRITE_TO
-
-
-    # spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-    # cs = digitalio.DigitalInOut(board.D5)
-    # mcp = MCP.MCP3008(spi, cs)
-    # channel = AnalogIn(mcp, MCP.P0)
     
 
     try:
-        mqtt_client = set_mqtt_connection()
+        for i in range(SERVO_NUM):
+            mqtt_client = set_mqtt_connection()
+            servo_thread = ServoThread('servo', GPIO, SERVO_PINS[i], mqtt_client, READ_TOPIC)
+            servo_threads.append(servo_thread)
 
-        servo_thread = ServoThread('servo', servo, mqtt_client, READ_TOPIC)
-        servo_thread.start()
+        for i in range(SERVO_NUM):
+            servo_threads[i].start()
+            
 
-        
 
 
     except (KeyboardInterrupt, SystemExit) as exErr:
